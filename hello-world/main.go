6577ddad923e9f6f72919e27ee5e1301f5789d65
bnchou <7933861+bnchou@users.noreply.github.com>
2023-12-31 13:53:43 +0000

    Manage language

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 // Domain code
-func Hello(name string) string {
+func Hello(name, language string) string {
 	const englishHelloPrefix = "Hello, "
+	const spanishHelloPrefix = "Hola, "
 
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
--- hello-world/main_test.go
+++ hello-world/main_test.go
@@ -4,14 +4,20 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		wanted := "Hello, Chris"
 
 		assertMessage(got, wanted, t)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		wanted := "Hello, World"
+
+		assertMessage(got, wanted, t)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		wanted := "Hola, Elodie"
 
 		assertMessage(got, wanted, t)
 	})
