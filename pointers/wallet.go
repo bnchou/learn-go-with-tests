d3010e0c41a4ae575c6799a7bbe8c8631ccca7de
bnchou <7933861+bnchou@users.noreply.github.com>
2024-01-02 21:17:46 +0000

    Add Withdraw

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -9,8 +9,11 @@ type Wallet struct {
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
