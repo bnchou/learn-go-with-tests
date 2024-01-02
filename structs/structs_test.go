fe10388887daf68b20e205734b1480fbb51c1752
bnchou <7933861+bnchou@users.noreply.github.com>
2024-01-02 20:46:47 +0000

    Introduce interface

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,20 +14,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Area of a rectangle", func(t *testing.T) {
-		got := Rectangle{Width: 20.0, Height: 10.0}.Area()
-		want := 200.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-	t.Run("Area of circle", func(t *testing.T) {
-		got := Circle{10}.Area()
-		want := 314.1592653589793
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("Area of a rectangle", func(t *testing.T) {
+		checkArea(t, Rectangle{Width: 20.0, Height: 10.0}, 200.0)
+	})
+	t.Run("Area of circle", func(t *testing.T) {
+		checkArea(t, Circle{10}, 314.1592653589793)
 	})
 }
