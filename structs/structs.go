d10acf5da26bfaedddffa99fd906280da9cf4eb0
bnchou <7933861+bnchou@users.noreply.github.com>
2024-01-02 20:43:24 +0000

    Add circle test

--- structs/structs.go
+++ structs/structs.go
@@ -1,13 +1,23 @@
 package structs
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+import "math"
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 type Rectangle struct {
 	Width, Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -4,19 +4,30 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	t.Run("Perimeter of a rectangle", func(t *testing.T) {
-		got := Perimeter(Rectangle{Width: 10.0, Height: 10.0})
+		got := Rectangle{Width: 10.0, Height: 10.0}.Perimeter()
 		want := 40.0
 
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
+}
+
+func TestArea(t *testing.T) {
 	t.Run("Area of a rectangle", func(t *testing.T) {
-		got := Area(Rectangle{Width: 20.0, Height: 10.0})
+		got := Rectangle{Width: 20.0, Height: 10.0}.Area()
 		want := 200.0
 
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("Area of circle", func(t *testing.T) {
+		got := Circle{10}.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
 		}
 	})
 }
