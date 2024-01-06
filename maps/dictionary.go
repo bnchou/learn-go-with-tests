5fdca352ebdccfda5837802f345f9c303dee9241
bnchou <7933861+bnchou@users.noreply.github.com>
2024-01-06 15:31:56 +0000

    Use switch

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,18 +4,25 @@ import (
 	"errors"
 )
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
-var ErrWordExists = errors.New("definition already exist")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("definition already exist")
+)
 
 type Dictionary map[string]string
 
 func (d Dictionary) Add(key, value string) error {
-	if _, ok := d[key]; ok {
-		return ErrWordExists
-	}
+	_, err := d.Search(key)
 
-	d[key] = value
-	return nil
+	switch err {
+	case nil:
+		return ErrWordExists
+	case ErrNotFound:
+		d[key] = value
+		return nil
+	default:
+		return err
+	}
 }
 
 func (d Dictionary) Search(s string) (string, error) {
