8251cb3dbf7190caa943772b64c4a125df65092d
bnchou <7933861+bnchou@users.noreply.github.com>
2024-01-06 15:29:26 +0000

    return error in case of already exist

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,11 +5,17 @@ import (
 )
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
+var ErrWordExists = errors.New("definition already exist")
 
 type Dictionary map[string]string
 
-func (d Dictionary) Add(key, value string) {
+func (d Dictionary) Add(key, value string) error {
+	if _, ok := d[key]; ok {
+		return ErrWordExists
+	}
+
 	d[key] = value
+	return nil
 }
 
 func (d Dictionary) Search(s string) (string, error) {
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -24,25 +24,47 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+		err := dictionary.Add(word, definition)
+
+		assertError(err, nil, t)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(err, ErrWordExists, t)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
-
-	assertString(got, want, t)
+	assertString(got, definition, t)
 }
 
-func assertError(got, want error, t *testing.T) {
+func assertError(got, want error, t testing.TB) {
+	t.Helper()
 	if got != want {
 		t.Fatalf("expected %q but got %q", want, got)
 	}
 }
 
-func assertString(got, want string, t *testing.T) {
+func assertString(got, want string, t testing.TB) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given, %q", got, want, "test")
 	}
