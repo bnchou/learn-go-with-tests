e40554c47256fc3ec8aacb06ccc48d449dd56c3e
bnchou <7933861+bnchou@users.noreply.github.com>
2024-01-06 15:12:09 +0000

    Better handling

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -7,9 +7,9 @@ import (
 type Dictionary map[string]string
 
 func (d Dictionary) Search(s string) (string, error) {
-	res := d[s]
-	if res == "" {
+	res, ok := d[s]
+	if !ok {
 		return "", errors.New("could not find the word you were looking for")
 	}
-	return d[s], nil
+	return res, nil
 }
