c9321e182def018bfa7a9f344c40334c11c27e2d
bnchou <7933861+bnchou@users.noreply.github.com>
2024-01-16 23:12:23 +0000

    Test with mock

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -7,15 +7,21 @@ import (
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleep := MockSleep{}
 
-	Countdown(buffer)
+	Countdown(buffer, &spySleep)
 
 	got := buffer.String()
 	want := `3
 2
 1
 Go!`
+
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleep.call != 3 {
+		t.Errorf("expected 3 calls but got %v", spySleep.call)
 	}
 }
--- mocking/main.go
+++ mocking/main.go
@@ -4,18 +4,38 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+type MockSleep struct {
+	call int
+}
+
+func (m *MockSleep) Sleep() {
+	m.call++
 }
