2786c481c61fc15e607c073d43732f534a7cb5ad
bnchou <7933861+bnchou@users.noreply.github.com>
2024-02-03 22:13:30 +0000

    Using select

--- select/racer.go
+++ select/racer.go
@@ -2,22 +2,22 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
-func Racer(u1, u2 string) string {
-	duration1 := measureGet(u1)
-	duration2 := measureGet(u2)
-
-	if duration1 > duration2 {
-		return u2
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
 	}
-
-	return u1
 }
 
-func measureGet(url string) time.Duration {
-	start1 := time.Now()
-	http.Get(url)
-	return time.Since(start1)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
