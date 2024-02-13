2c1ad208cd872983a2f0b5e377542f06be56b1e4
bnchou <7933861+bnchou@users.noreply.github.com>
2024-02-13 22:25:50 +0000

    Refactor reflection

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,7 +7,7 @@ import (
 func walk(x any, f func(input string)) {
 	v := reflect.VisibleFields(reflect.TypeOf(x))
 	for _, vf := range v {
-		if vf.Type.Name() == "string" {
+		if vf.Type.Kind() == reflect.String {
 			f(reflect.ValueOf(x).FieldByName(vf.Name).String())
 		}
 	}
