da2604b90a209966735abf1d16a4e14f31f0ce86
bnchou <7933861+bnchou@users.noreply.github.com>
2024-02-13 23:09:06 +0000

    Func

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -30,6 +30,23 @@ func assertContains(t testing.TB, haystack []string, needle string) {
 
 func TestWalk(t *testing.T) {
 
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
 	t.Run("with channels", func(t *testing.T) {
 		aChannel := make(chan Profile)
 
