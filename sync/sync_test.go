e9bfc420eaca8809ca0b1234468e8d17e3daef21
bnchou <7933861+bnchou@users.noreply.github.com>
2024-02-15 22:03:38 +0000

    Refactor

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,25 +1,58 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
 		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func assertCounter(t *testing.T, counter *Counter, i int) {
+	t.Helper()
+	if counter.Value() != i {
+		t.Errorf("got %d, want %d", counter.Value(), i)
+	}
+}
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
