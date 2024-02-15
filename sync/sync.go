9ec18903156d6c81970bab201c050aa482ab9dca
bnchou <7933861+bnchou@users.noreply.github.com>
2024-02-15 22:04:19 +0000

    Divide sync file

--- sync/sync.go
+++ sync/sync.go
@@ -1,1 +1,32 @@
 package sync
+
+import (
+	"sync"
+	"testing"
+)
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func assertCounter(t *testing.T, counter *Counter, i int) {
+	t.Helper()
+	if counter.Value() != i {
+		t.Errorf("got %d, want %d", counter.Value(), i)
+	}
+}
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
