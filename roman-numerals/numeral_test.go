bb2fefcb3ba013a10d9e8494536f9e2bf8ca32f3
bnchou <7933861+bnchou@users.noreply.github.com>
2024-03-29 22:02:29 +0000

    ConvertToArabic

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -10,7 +11,7 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var allRomansNumerals = []RomanNumeral{
+var allRomanNumerals = []RomanNumeral{
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -60,7 +61,7 @@ func TestRomanNumerals(t *testing.T) {
 
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
-	for _, v := range allRomansNumerals {
+	for _, v := range allRomanNumerals {
 		for arabic >= v.Value {
 			result.WriteString(v.Symbol)
 			arabic -= v.Value
@@ -68,4 +69,49 @@ func ConvertToRoman(arabic int) string {
 	}
 
 	return result.String()
+}
+
+func ConvertToArabic(roman string) int {
+	var arabic = 0
+
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+
+	return arabic
+}
+
+func TestConvertingToArabic(t *testing.T) {
+	cases := []struct {
+		Description string
+		Arabic      int
+		Roman       string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
+		{"6 gets converted to VI", 6, "VI"},
+		{"9 gets converted to IX", 9, "IX"},
+		{"10 gets converted to X", 10, "X"},
+		{"14 gets converted to XIV", 14, "XIV"},
+		{"40 gets converted to XL", 40, "XL"},
+		{"47 gets converted to XLVII", 47, "XLVII"},
+		{"49 gets converted to XLIX", 49, "XLIX"},
+		{"50 gets converted to L", 50, "L"},
+		{Arabic: 1984, Roman: "MCMLXXXIV"},
+	}
+
+	for _, test := range cases[:10] {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
+			}
+		})
+	}
 }
