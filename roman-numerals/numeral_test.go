f11596c35294070c0cebc488d2bc0256326156b6
bnchou <7933861+bnchou@users.noreply.github.com>
2024-03-29 21:32:00 +0000

    v2

--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -3,14 +3,32 @@ package main
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Roman       string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+	}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
+			}
+		})
 	}
 }
 
 func ConvertToRoman(i int) string {
+	if i == 3 {
+		return "III"
+	}
+	if i == 2 {
+		return "II"
+	}
 	return "I"
 }
